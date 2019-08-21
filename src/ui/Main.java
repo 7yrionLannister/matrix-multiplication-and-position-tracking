e2c52d91e4bdeac4c1db9ef2c398ca3f804b933c
Daniel Fernandez <alejo.dafr@gmail.com>
2019-08-20 19:14:43 -0500

    Added missing class diagram

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -29,7 +29,7 @@ public class Main extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
-		int[][] A = new int[][] {{1,2,3,4,5,6,7,8},
+		/*int[][] A = new int[][] {{1,2,3,4,5,6,7,8},
 			{8,7,6,5,4,3,2,1},
 			{2,2,2,2,2,2,2,2},
 			{3,3,3,3,3,3,3,3},
@@ -68,7 +68,7 @@ public class Main extends Application {
 				System.out.print(C[i][j] + " ");
 			}
 			System.out.println();
-		}
+		}*/
 	}
 }
 
